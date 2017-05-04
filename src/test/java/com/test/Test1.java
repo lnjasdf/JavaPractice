84c093ccbdf9a31db5b2f594ca5f7d9fa96411c2
lnjasdf <lnjasdf@qq.com>
2017-05-04 16:07:34 +0800

    ADD: 增加设计模式文档 MOD: 修改java语言版本1.7

--- src/test/java/com/test/Test1.java
+++ src/test/java/com/test/Test1.java
@@ -1,12 +1,23 @@
 package com.test;
 
-import org.junit.Test;
+import static java.lang.System.out;
 
 /**
  * Created by intron on 2016/6/26 0026.
  */
-public class Test1 {
+public class Test1 implements AutoCloseable{
+
+
     static public void main(String[] args) {
-        System.out.println("hello world");
+        try (Test1 t = new Test1()) {
+            out.println("hello world");
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        out.println("close");
     }
 }
